1d798b715e628c1cae3509f0bc30ac169f08eeba
lyly2112 <31433730+lyly2112@users.noreply.github.com>
2021-07-27 22:58:33 -0700

    Add fixed effect to the Navbar component.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,30 +4,38 @@ import { animateScroll as scroll } from "react-scroll";
 
 function Navbar() {
   return (
-    <div className="navbar-menu">
-      <div className="navbar-menu__element">
-        <div className="navbar-menu__text" onClick={() => scroll.scrollToTop()}>
-          Home
+    <div className="navbar">
+      <div className="navbar__column--left">
+        <div className="navbar__element">
+          <div className="navbar__text" onClick={() => scroll.scrollToTop()}>
+            Liliya Sherstobitova
+          </div>
         </div>
       </div>
-      <div className="navbar-menu__element">
-        <Link
-          to="about"
-          smooth={true}
-          duration={1000}
-          className="navbar-menu__text"
-        >
+      <div className="navbar__column--right"></div>
+      <div className="navbar__element">
+        <Link to="about" smooth={true} duration={1000} className="navbar__text">
           About
         </Link>
       </div>
-      <div className="navbar-menu__element">
+      <div className="navbar__element">
         <Link
           to="portfolio"
           smooth={true}
           duration={1000}
-          className="navbar-menu__text"
+          className="navbar__text"
         >
           Portfolio
+        </Link>
+      </div>
+      <div className="navbar__element">
+        <Link
+          to="footer"
+          smooth={true}
+          duration={1000}
+          className="navbar__text"
+        >
+          Links
         </Link>
       </div>
     </div>
