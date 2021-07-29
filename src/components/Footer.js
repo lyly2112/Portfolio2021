9fec14496c05ee096d33c6e30330fba809d5eac2
lyly2112 <31433730+lyly2112@users.noreply.github.com>
2021-07-29 00:32:55 -0700

    Update Footer component style.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,12 +6,15 @@ import linkedin from "../images/icons/linkedin.png";
 function Footer() {
   return (
     <div className="footer">
-      <ExternalLink href="https://github.com/lyly2112">
-        <img className="footer__icon" src={git} alt="github" />
-      </ExternalLink>
-      <ExternalLink href="https://www.linkedin.com/in/liliya-sherstobitova-322a4b125/">
-        <img className="footer__icon" src={linkedin} alt="linkedin" />
-      </ExternalLink>
+      <div className="footer__links">
+        <ExternalLink href="https://github.com/lyly2112">
+          <img className="footer__icon" src={git} alt="github" />
+        </ExternalLink>
+        <ExternalLink href="https://www.linkedin.com/in/liliya-sherstobitova-322a4b125/">
+          <img className="footer__icon" src={linkedin} alt="linkedin" />
+        </ExternalLink>
+      </div>
+      <div className="footer__copyright">	&copy; Liliya Sherstobitova</div>
     </div>
   );
 }
