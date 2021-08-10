1d73560481c6b8cef603f6b5b92223f5d429ad44
lyly2112 <31433730+lyly2112@users.noreply.github.com>
2021-08-10 00:46:56 -0700

    Change footer layoutsettings.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { ExternalLink } from "react-external-link";
-import git from "../images/icons/git.svg";
+import github from "../images/icons/github.svg";
 import linkedin from "../images/icons/linkedin.svg";
 
 function Footer() {
@@ -8,7 +8,7 @@ function Footer() {
     <div className="footer">
       <div className="footer__links">
         <ExternalLink href="https://github.com/lyly2112">
-          <img className="footer__icon" src={git} alt="github" />
+          <img className="footer__icon" src={github} alt="github" />
         </ExternalLink>
         <ExternalLink href="https://www.linkedin.com/in/liliya-sherstobitova-322a4b125/">
           <img className="footer__icon" src={linkedin} alt="linkedin" />
