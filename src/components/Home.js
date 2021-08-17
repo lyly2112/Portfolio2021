776a64fde1d2ae64b352edcae2a9deeb29d7b695
lyly2112 <31433730+lyly2112@users.noreply.github.com>
2021-08-17 02:11:48 -0700

    Add animation to the header in Home component.

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,41 +6,60 @@ import { animateScroll as scroll } from "react-scroll";
 import { Link } from "react-scroll";
 
 function Home() {
-  const bounce = keyframes`
-  0% {opacity: 1; } 
-  50% {opacity: 0.4;}
-   100% {opacity: 1;} 
-    0% {transform: translateY(0px);  } 
-    50% {transform: translateY(-30px);  } 
-    100% {transform: translateY(0px);  }    
+  const nameAnimation = keyframes`
+  0% {
+    opacity: 0;
+    -webkit-transform: translateY(-75px);
+    transform: translateY(-75px);
+}
+
+10% {
+    opacity: 0;
+    -webkit-transform: translateY(-75px);
+    transform: translateY(-75px);
+}
+100% {
+    opacity: 1;
+    -webkit-transform: translate(0);
+    transform: translate(0);
+}
   `;
 
-  const Bounce = styled.div`
-    animation: ${bounce} 3s linear infinite;
+  const NameAnimation = styled.h2`
+    animation: ${nameAnimation} 2s;
   `;
 
-  // const GlowInArrow = styled.div`
-  // :hover {
-  //     filter: drop-shadow(0px 5px 2px rgb(0 0 0 / 1));
-  //     opacity: 1;
-  //   }
-  // `;
+  const arrowAnimation = keyframes`
+  0% {opacity: 1; 
+  transform: translateY(0px); } 
+  50% {opacity: 0.4;
+  transform: translateY(-30px);}
+   100% {opacity: 1;
+  transform: translateY(0px); 
+  } 
+ 
+  `;
+
+  const ArrowAnimation = styled.div`
+    animation: ${arrowAnimation} 3s linear infinite;
+  `;
 
   return (
     <div className="home">
       <div className="home__header">
         {/* add drop effect */}
-        <div className="home__header-name">Liliya Sherstobitova</div>
+        <NameAnimation>
+          <h2 className="home__header-name">Liliya Sherstobitova</h2>
+        </NameAnimation>
         {/* add follow up fade effect */}
         <div className="home__header-title">WEB developer</div>
       </div>
-
       <Link to="about" smooth={true} duration={1000} className="navbar__text">
-        <Bounce id="home__arrow">
+        <ArrowAnimation id="home__arrow">
           <div className="home__arrow">
             <img className="home__arrow-down" src={arrow} alt="see more" />
           </div>
-        </Bounce>
+        </ArrowAnimation>
       </Link>
     </div>
   );
