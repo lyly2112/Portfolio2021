0d8de362867dcfe359a6f502a2112de5bc35605c
lyly2112 <31433730+lyly2112@users.noreply.github.com>
2021-09-07 15:58:42 -0700

    Change style of the icons and add email icon to the Footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,20 @@ import React from "react";
 import { ExternalLink } from "react-external-link";
 import github from "../images/icons/github.svg";
 import linkedin from "../images/icons/linkedin.svg";
+import email from "../images/icons/email.svg";
 
 function Footer() {
   return (
     <div className="footer">
       <div className="footer__links">
-        <ExternalLink href="https://github.com/lyly2112">
-          <img className="footer__icon" src={github} alt="github" />
+        <ExternalLink  className="footer__icon" href="https://github.com/lyly2112">
+          <img src={github} alt="github" />
         </ExternalLink>
-        <ExternalLink href="https://www.linkedin.com/in/liliya-sherstobitova-322a4b125/">
-          <img className="footer__icon" src={linkedin} alt="linkedin" />
+        <ExternalLink  className="footer__icon" href="https://www.linkedin.com/in/liliya-sherstobitova-322a4b125/">
+          <img src={linkedin} alt="linkedin" />
+        </ExternalLink>
+        <ExternalLink className="footer__icon" href="mailto: lyly2112@gmail.com">
+          <img  src={email} alt="email" />
         </ExternalLink>
       </div>
       <h2 className="footer__copyright">	&copy; Liliya Sherstobitova</h2>
