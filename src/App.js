09218bc9d2039a9f6b8b54d257dbf6539e639112
lyly2112 <31433730+lyly2112@users.noreply.github.com>
2021-09-14 09:09:04 -0700

    Add id parameter to the App component.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Footer from "./components/Footer"
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" id="app">
         <Navbar />
         <Home />
         <About />
