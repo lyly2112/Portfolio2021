e0cbfb96b60578797c410ac185ccedb150f4514f
lyly2112 <31433730+lyly2112@users.noreply.github.com>
2021-09-18 22:39:34 -0700

    Change icons in the footer component.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,20 +6,35 @@ import email from "../images/icons/email.svg";
 
 function Footer() {
   return (
-    <div className="footer">
-      <div className="footer__links">
-        <ExternalLink  className="footer__icon" href="https://github.com/lyly2112">
-          <img src={github} alt="github" />
-        </ExternalLink>
-        <ExternalLink  className="footer__icon" href="https://www.linkedin.com/in/liliya-sherstobitova-322a4b125/">
-          <img src={linkedin} alt="linkedin" />
-        </ExternalLink>
-        <ExternalLink className="footer__icon" href="mailto: lyly2112@gmail.com">
-          <img  src={email} alt="email" />
-        </ExternalLink>
-      </div>
-      <h2 className="footer__copyright">	&copy; Liliya Sherstobitova</h2>
-    </div>
+    <nav className="footer">
+      <ul className="footer__links">
+        <li>
+          <ExternalLink
+            // className="footer__icon"
+            href="https://github.com/lyly2112"
+          >
+            <img src={github} alt="github" className="footer__icon" />
+          </ExternalLink>
+        </li>
+        <li>
+          <ExternalLink
+            // className="footer__icon"
+            href="https://www.linkedin.com/in/liliya-sherstobitova-322a4b125/"
+          >
+            <img src={linkedin} alt="linkedin" className="footer__icon" />
+          </ExternalLink>
+        </li>
+        <li>
+          <ExternalLink
+            // className="footer__icon"
+            href="mailto: lyly2112@gmail.com"
+          >
+            <img src={email} alt="email" className="footer__icon" />
+          </ExternalLink>
+        </li>
+      </ul>
+      <h2 className="footer__copyright"> &copy; Liliya Sherstobitova</h2>
+    </nav>
   );
 }
 
