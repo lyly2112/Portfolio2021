0b4fc0f9724b5605caad87d8900b4e79ebf84a78
lyly2112 <31433730+lyly2112@users.noreply.github.com>
2021-09-24 21:20:14 -0700

    Remove id for App component.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Footer from "./components/Footer"
 class App extends Component {
   render() {
     return (
-      <div className="App" id="app">
+      <div className="App">
         <Navbar />
         <Home />
         <About />
