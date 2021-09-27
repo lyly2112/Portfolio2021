bfc1561e42682e44a10c603c42b80f406b1f4c35
lyly2112 <31433730+lyly2112@users.noreply.github.com>
2021-09-27 06:58:44 -0700

    Change Navbar css style.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,6 @@ import { MdClose } from "react-icons/md";
 import { FiMenu } from "react-icons/fi";
 
 function Navbar() {
-
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   const handleToggle = () => {
@@ -18,11 +17,12 @@ function Navbar() {
   };
 
   return (
-    <header className="navbar">
-      <h2 className="navbar-logo" onClick={() => scroll.scrollToTop()}>
-        Liliya Sherstobitova
-      </h2>
-      <div className="navbar-menu">
+    <div className="navbar-wrap">
+      <header className="navbar">
+        <h2 className="navbar-logo" onClick={() => scroll.scrollToTop()}>
+          Liliya Sherstobitova
+        </h2>
+        {/* <div className="navbar-menu"> */}
         <nav className="navbar-desktop">
           <ul className="navbar-desktop-list">
             <li className="navbar-desktop-element">
@@ -59,53 +59,56 @@ function Navbar() {
         </nav>
 
         <nav className="navbar-mobile">
-        <button onClick={handleToggle}>
-          {navbarOpen ? (
-            <MdClose style={{ color: "#fff", width: "40px", height: "40px" }} />
-          ) : (
-            <FiMenu
-              style={{ color: "#fff", width: "40px", height: "40px" }}
-            />
-          )}
-        </button>
-        <ul className={`navbar-side-panel ${navbarOpen ? " showMenu" : ""}`}>
-          <li className="navbar-mobile-element">
-            <Link
-              to="about"
-              smooth={true}
-              duration={1000}
-              className="navbar-mobile-element__text"
-              onClick={() => closeMenu()}
-            >
-              About
-            </Link>
-          </li>
-          <li className="navbar-mobile-element">
-            <Link
-              to="portfolio"
-              smooth={true}
-              duration={1000}
-              className="navbar-mobile-element__text"
-              onClick={() => closeMenu()}
-            >
-              Portfolio
-            </Link>
-          </li>
-          <li className="navbar-mobile-element">
-            <Link
-              to="footer"
-              smooth={true}
-              duration={1000}
-              className="navbar-mobile-element__text"
-              onClick={() => closeMenu()}
-            >
-              Links
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      </div>
-    </header>
+          <button onClick={handleToggle}>
+            {navbarOpen ? (
+              <MdClose
+                style={{ color: "#fff", width: "40px", height: "40px" }}
+              />
+            ) : (
+              <FiMenu
+                style={{ color: "#fff", width: "40px", height: "40px" }}
+              />
+            )}
+          </button>
+          <ul className={`navbar-side-panel ${navbarOpen ? " showMenu" : ""}`}>
+            <li className="navbar-mobile-element">
+              <Link
+                to="about"
+                smooth={true}
+                duration={1000}
+                className="navbar-mobile-element__text"
+                onClick={() => closeMenu()}
+              >
+                About
+              </Link>
+            </li>
+            <li className="navbar-mobile-element">
+              <Link
+                to="portfolio"
+                smooth={true}
+                duration={1000}
+                className="navbar-mobile-element__text"
+                onClick={() => closeMenu()}
+              >
+                Portfolio
+              </Link>
+            </li>
+            <li className="navbar-mobile-element">
+              <Link
+                to="footer"
+                smooth={true}
+                duration={1000}
+                className="navbar-mobile-element__text"
+                onClick={() => closeMenu()}
+              >
+                Links
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        {/* </div> */}
+      </header>
+    </div>
   );
 }
 
